3416d33335a5c4e74dbde450b4abc41330ad2dd1
xb1002 <2709668323@qq.com>
2024-11-23 12:36:14 +0800

    微调

--- src/config.py
+++ src/config.py
@@ -4,8 +4,6 @@ DOMAIN_API_ENDPOINTS = {
     "PING": [
         "http://13.215.134.222/api/network/ping",
         "http://18.139.20.49/api/network/ping",
-        "http://52.74.35.173/api/network/ping",
-        "http://52.77.10.116/api/network/ping",
         "http://3.1.154.253/api/network/ping"
     ]
 }
